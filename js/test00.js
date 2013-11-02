1b7a228acc7affd3dab641a7b7cef18c61272bef
2707 <kaka@tadalih.org>
2013-11-02 16:35:53 +0700

    add incidents link but doesn't work

--- js/test00.js
+++ js/test00.js
@@ -1,24 +1,48 @@
 $(document).ready(function(){
-	var url = "http://korupedia.org/api?task=categories";
+	
+	//var url = "http://korupedia.org/api?task=categories";
+	var url1 = tarikData("kategori");
+	var url2 = tarikData("insiden");
+    
+	
 	$.ajax({
-				url: url,
+				url: url1,
 				dataType: 'jsonp',
 				jsonpCallback: 'Array',
 				contentType: 'application/json',
 				success: function(data) {
+
 					successCallback(data);
 				}
 	});
 	
 });
 
+function tarikData (tautAn) {
+	var url;
+	  
+	switch(tautAn) {
+	     case 'kategori':
+			    url = "http://korupedia.org/api?task=categories";
+		 break;
+				   
+	     case 'kejadian':
+                url = "http://korupedia.org/api?task=incidents";
+		 break;
+					             
+		 default:break
+						               
+		}
+
+	return url;
+};
+
 function successCallback (data) {
-	// body...
+	// retrieve message from ajax and display to frontend
+	
 	$.each(data.payload["categories"], function(datax, item){
 		var kat= item.category.title;
-		$(".result").append( kat );
+		$(".result").append('<ul><li>' + kat + '</li></ul>' );
 		
-	});
-	
-	
+	});	
 };
